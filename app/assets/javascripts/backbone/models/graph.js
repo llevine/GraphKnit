f13e1a0233a510b320d10d53422a0d1e5e1a2d7f
llevine <LaurenLevineDesigns@gmail.com>
2015-01-30 16:41:04 -0500

    removed some unused code. added more space to the top margin of the forms

--- app/assets/javascripts/backbone/models/graph.js
+++ app/assets/javascripts/backbone/models/graph.js
@@ -1,20 +1,5 @@
-// function defaultGraph() {
-// 	var a = [];
-// 	a.length = 40;
-// 	for (var i = 0; i < 40; i++) {
-// 		var b = [];
-// 		b.length = 40;
-// 		for (var j = 0; j < 40; j++) {
-// 			b[j] = "#fff";
-// 		}
-// 		a[i] = b;
-// 	}
-// 	return a;
-// }
-
 App.Models.Graph = Backbone.Model.extend({
 	urlRoot: '/graphs',
-	//url: function() { return '/graphs'; },
 	initialize: function(){
 		console.log('created new graph model');
 	},
