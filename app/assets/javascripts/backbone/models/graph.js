44b5f8c0320b9fddeea4b2f3e85c59e7ad3db4ee
llevine <LaurenLevineDesigns@gmail.com>
2015-02-01 17:03:28 -0500

    reworking code to exclude backbone

--- app/assets/javascripts/backbone/models/graph.js
+++ app/assets/javascripts/backbone/models/graph.js
@@ -1,24 +1,24 @@
-App.Models.Graph = Backbone.Model.extend({
-	urlRoot: '/graphs',
-	initialize: function(){
-		console.log('created new graph model');
-	},
-	defaults: {
-		user_id: null,
-		name: 'My First Graph',
-		category: 'My Category',
-		image_url: '',
-		product_image: '',
-		// delete difficulty for now
-		// difficulty: null,
-		// gauge: 0,
-		// rows: 100,
-		// columns: 100,
-		// delete number of colors
-		// number_of_colors: 1,
-		layout: '|',
-		notes: 'Graph notes',
-		privacy: false,
-		preview: ''
-	}
-});
+// App.Models.Graph = Backbone.Model.extend({
+// 	urlRoot: '/graphs',
+// 	initialize: function(){
+// 		console.log('created new graph model');
+// 	},
+// 	defaults: {
+// 		user_id: null,
+// 		name: 'My First Graph',
+// 		category: 'My Category',
+// 		image_url: '',
+// 		product_image: '',
+// 		// delete difficulty for now
+// 		// difficulty: null,
+// 		// gauge: 0,
+// 		// rows: 100,
+// 		// columns: 100,
+// 		// delete number of colors
+// 		// number_of_colors: 1,
+// 		layout: '|',
+// 		notes: 'Graph notes',
+// 		privacy: false,
+// 		preview: ''
+// 	}
+// });
